00b62828ab61698ce3d60a31aa27762a4bb004c0
Dipesh Gyawali <dipeshgyawali365@gmail.com>
2023-04-18 17:42:53 +0545

    dynamic add to cart and remove

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -4,20 +4,21 @@ import {CartCard} from "../components";
 import { useCart } from '../context/CartContext';
 
 export const Cart = ({title}) => {
-  const { total } = useCart();
+
+  const { total, cartList } = useCart();
 
   useTitle(title);
 
-  const products = [
-    {"id": 1, "name": "Sony Wh-Ch510 Bluetooth Wireless", "price": 149, "image": "src/assets/images/book.jpg"},
-    {"id": 2, "name": "boAt Rockerz 450", "price": 49, "image": "src/assets/images/headphones.jpg"},
-  ]
+  // console.log(cartList);
+
+
+
 
   return (
     <main>
       <section className="cart">
-        <h1>Cart Items: {products.length}/ ${total}</h1>
-        { products.map((product) => (
+        <h1>Cart Items: {cartList.length}/ ${total}</h1>
+        { cartList.map((product) => (
         <CartCard key={product.id} product={product}/>
         )) }
       </section>
